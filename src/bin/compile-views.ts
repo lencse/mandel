c6affd267f72b27964a3e504cf767d74f04f4243
Levente Löki <leventeloki@gmail.com>
2017-08-29 19:38:10 +0200

    Dist build

--- src/bin/compile-views.ts
+++ src/bin/compile-views.ts
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
-import { compile }  from 'handlebars';
-import * as commandLineArgs from 'command-line-args';
-import * as fs from 'fs';
-import * as path from 'path';
-import dirs from './dirs';
+import { compile }  from 'handlebars'
+import * as commandLineArgs from 'command-line-args'
+import * as fs from 'fs'
+import * as path from 'path'
+import dirs from './dirs'
 
 const options = commandLineArgs([{
     name: 'watch',
     alias: 'w',
     type: Boolean,
     defaultValue: false
-}]);
+}])
 
 const template = fs.readFileSync(path.resolve(dirs.projectRoot, dirs.views ,'index.hbs')).toString()
-const targetDir = options.watch ? dirs.dist.watch : dirs.dist.prod
-
 const content = compile(template)({
     watch: options.watch
 })
 
-fs.writeFileSync(path.resolve(targetDir, 'index.html'), content)
+const targetDir = path.resolve(dirs.projectRoot, options.watch ? dirs.dist.watch : dirs.dist.prod)
 
+fs.mkdirSync(targetDir)
+fs.writeFileSync(path.resolve(targetDir, 'index.html'), content)
